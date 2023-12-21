e79ea04d022563631704bcc2e3ff01395a367f3e
Chafic Nehme <cnehm006@uottawa.ca>
2023-12-20 20:45:32 -0500

    basic stuff

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('dealButton').addEventListener('click', deal);
+document.getElementById('hitButton').addEventListener('click', hit);
+document.getElementById('standButton').addEventListener('click', stand);
+
+function deal() {
+    console.log('Deal cards');
+    // Add functionality to deal cards
+}
+
+function hit() {
+    console.log('Player hits');
+    // Add functionality for player hit
+}
+
+function stand() {
+    console.log('Player stands');
+    // Add functionality for player stand
+}
